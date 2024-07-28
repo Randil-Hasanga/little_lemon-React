c0aac27ef881088735ed04b626048355887fdf36
Randil Hasanga <randil_2020690@fot.ruh.ac.lk>
2024-07-28 11:37:14 +0530

    about page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Header/>
       <SpecialMenu/>
       <Reviews/>
+      <About/>
     </main>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import image1 from '../assets/images/chef.4b4c2d26b514db32e8fc.jpg'
+import image2 from '../assets/images/chefs.6ba0e06bfa9c1ea75109.jpg'
 
 const About = () => {
     return (
-        <div id='about' className='about'>
-            About
-        </div>
+        <section className='about'>
+            <div className='about-text'>
+                <h1 className='m-font'>About Us</h1>
+                <p>Welcome to <strong>Little Lemon</strong>, Sri Lanka's premier family-owned Mediterranean restaurant. We blend traditional recipes with a modern twist, using the freshest local ingredients and time-honored cooking techniques. Our menu celebrates the rich flavors of the Mediterranean, offering a delightful culinary experience in a warm and welcoming atmosphere. Join us for a meal and experience the perfect harmony of tradition and innovation.</p>
+            </div>
+            <img src={image2} alt="image 1" className='image image1'/>
+            <img src={image1} alt="image 1" className='image image2'/>
+        </section>
     );
 };
 
